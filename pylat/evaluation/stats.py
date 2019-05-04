d160d4a2a5382b895c282a767fa74e6553b45543
Alejandro González Hevia <alejandrgh11@gmail.com>
2019-05-04 16:54:21 +0200

    Added documentation of stats module

--- pylat/evaluation/stats.py
+++ pylat/evaluation/stats.py
@@ -16,6 +16,38 @@ confidence_interval_to_const = {
 
 
 def wilson_score_interval(error, n, confidence):
+    """ Calculates the Wilson Score interval of a model with a given confidence.
+
+    Parameters
+    ----------
+    error : float
+        Error score of the classifier with the sample data. It must be a float
+        between 0 and 1.
+    n : int
+        Number of samples in the data.
+    confidence : float
+        Value of confidence to be used when calculating the interval. Valid
+        values are 0.90, 0.95, 0.98 and 0.99.
+
+    Returns
+    -------
+    float
+        Float that
+
+    Examples
+    --------
+    >>> from pylat.evaluation.stats import wilson_score_interval
+    >>> n = 200  # 200 samples
+    >>> error = 0.15  # model misclassified 15% of the samples
+    >>> print(wilson_score_interval(error, n, 0.90))  # 90% confidence
+
+    >>> print(wilson_score_interval(error, n, 0.95))  # 95% confidence
+
+    >>> print(wilson_score_interval(error, n, 0.99))  # 99% confidence
+
+    >>> print(wilson_score_interval(error, n, 0.79))  # invalid confidence
+
+    """
     try:
         const = confidence_interval_to_const[confidence]
     except KeyError:
@@ -26,6 +58,32 @@ def wilson_score_interval(error, n, confidence):
 
 
 def mcnemar_test(y_pred1, y_pred2, y_true):
+    """Performs a McNemar test of two predictions compared to the true values.
+
+    This method can be used to check if there is a significant difference
+    between the predictions made by two different classifiers.
+
+    Notes
+    -----
+    This method should only be applied to compare the outputs of two binary
+    classifiers. In order to compare multiclass classifiers, please see
+    :func:`~stuart_maxwell_test`.
+
+    Parameters
+    ----------
+    y_pred1 : iterable
+        Iterable containing every prediction made by the first classifier.
+    y_pred2 : iterable
+        Iterable containing every prediction made by the second classifier.
+    y_true : iterable
+        Iterable contining the true predictions.
+
+    Returns
+    -------
+    float
+        Float bounded between 0 and 1, containing the result of applying the
+        McNemar test.
+    """
     classif1_correct = [1 if y_pred1[i] == y_true[i] else 0
                         for i, _ in enumerate(y_pred1)]
     classif2_correct = [1 if y_pred2[i] == y_true[i] else 0
@@ -35,6 +93,29 @@ def mcnemar_test(y_pred1, y_pred2, y_true):
 
 
 def stuart_maxwell_test(y_pred1, y_pred2):
+    """Perform a Stuart-Maxwell test to compare the results of two classifiers.
+
+    Notes
+    -----
+    This method is equivalent to calling :func:`~mcnemar_test` if called with the
+    predictions of two binary classifiers.
+
+    If the two predictions agree on more than 1 label (they produce the same
+    outpus for that label), this method will return an error since the
+    Stuart-Maxwell test can't be calculated in that case.
+
+    Parameters
+    ----------
+    y_pred1: iterable
+        Iterable containing every prediction made by the first classifier.
+    y_pred2
+        Iterable containing every prediction made by the second classifier.
+
+    Returns
+    -------
+    float
+        Float bounded between 0 and 1 containing the result of the
+    """
     conf_matrix = confusion_matrix(y_pred1, y_pred2)
     n = np.sum(conf_matrix, axis=1)
     n_prime = np.sum(conf_matrix, axis=0)
